2ba9cc4099ac10de1d19f7dc0e1ffa208dfb2af7
Synthwaver <makc-sam@mail.ru>
2019-01-10 19:16:38 +0700

    Realize simple html-server

---
+++ lesson5/src/main/java/ru/eltex/server/Server.java
@@ -0,0 +1,78 @@
+package ru.eltex.server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Server extends Thread {
+    private Socket socket;
+    private InputStream in;
+    private OutputStream out;
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(7777);
+            System.out.println("Server started");
+            while (true) {
+                new Server(server.accept());
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e);
+        }
+    }
+
+    private Server(Socket socket) throws IOException {
+        this.socket = socket;
+        in = socket.getInputStream();
+        out = socket.getOutputStream();
+        setDaemon(true);
+        start();
+    }
+
+    public void run() {
+        try {
+            String inputHeader = readInputHeaders();
+            String filename = inputHeader.split("/")[1].replace("HTTP", "").trim();
+            if (filename.length() <= 1) {
+                filename = "index.html";
+            }
+            System.out.println("File requested: " + filename);
+            byte[] byteArray = Files.readAllBytes(Paths.get(filename));
+            writeResponse(byteArray);
+        } catch (Exception e) {
+            System.err.println("Error: " + e);
+        }
+        try {
+            socket.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e);
+        }
+        System.out.println("Client processing finished");
+    }
+
+    private String readInputHeaders() throws IOException {
+        BufferedReader bufReader = new BufferedReader(new InputStreamReader(in));
+        StringBuilder strBuilder = new StringBuilder();
+        while (true) {
+            String line = bufReader.readLine();
+            if (line == null || line.trim().length() == 0) {
+                break;
+            }
+            strBuilder.append(line);
+        }
+        return strBuilder.toString();
+    }
+
+    private void writeResponse(byte[] byteArray) throws IOException {
+        String header = "HTTP/1.1 200 OK\r\n" +
+                "Content-Type: text/html; charset=utf-8\r\n" +
+                "Content-Length: " + byteArray.length +
+                "\r\n\r\n";
+
+        out.write(header.getBytes());
+        out.write(byteArray);
+        out.flush();
+    }
+}
